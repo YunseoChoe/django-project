c9eee6c9b069c73a5b88be607f9789c26838882f
choeyunseo <choeyunseo4060@naver.com>
2024-01-02 14:59:36 +0900

    update

--- posts/views.py
+++ posts/views.py
@@ -1,39 +1,56 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.views.generic.list import ListView
+from django.utils import timezone
 
 from .models import Post
+
 
 def index(request):
     return render(request, 'index.html')
 
+# 목록보기
 def post_list_view(request):
-    return render(request, 'posts/post_list.html')
+    post = Post.objects.all() # 모든 글을 가져옴
 
-def post_detail_view(request):
-    return render(request, 'posts/post_detail.html')
+    return render(request, 'posts/post_list.html', {'post': post})
 
+# 상세보기
+def post_detail_view(request, id):
+    # id에 해당하는 게시글이 없으면 404 에러 반환
+    post = get_object_or_404(Post, id=id)
+    
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+# 작성하기
 def post_create_view(request):
     if request.method == 'GET':
         return render(request, 'posts/post_form.html')
     else:
         image = request.FILES.get('image') # image는 file로 받아야 됨
         content = request.POST.get('content')
-        writer = request.POST.get('writer')
+        # created_at은 자동으로 현재 시간으로 설정
+        created_at = timezone.now(),
+        # 작성자는 현재 로그인한 사용자로 설정, 로그인 후 동작
+        # writer = request.user,
         print(image)
         print(content)
-        print(writer)
+        # print(writer)
+        print(created_at)
         # data 생성
         Post.objects.create(
             image = image,
             content = content,
-            writer = request.user # 로그인 후 동작됨
+            # writer = writer,
+            created_at = created_at,
         )
         return redirect('index')
 
+# 수정하기
 def post_update_view(request):
     return render(request, 'posts/post_form.html')
 
+# 삭제하기
 def post_delete_view(request):
     return render(request, 'posts/post_confirm_delete.html')
 
