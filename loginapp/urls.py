560f83958c65800ce61a5e6248a074d463ef2db2
choeyunseo <choeyunseo4060@naver.com>
2024-01-08 02:35:09 +0900

    Fix urls

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('', views.login, name='login'),
-    path('home/', views.home, name='home'), # login 성공하면 뜨는 화면
+    path('login/', views.login, name='login'),
+    path('afterLogin/', views.afterLogin, name='afterLogin'), # login 성공하면 뜨는 화면
 
     path('logout/', views.logout, name='logout'),
+    path('', views.home, name='home'),
 ] 
--- loginapp/views.py
+++ loginapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 def signup(request):
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
@@ -10,6 +13,9 @@ def signup(request):
                 password=request.POST['password1'],
                 email=request.POST['email'],
             )
+            print(f'username: {user.username}')
+            print(f'password: {user.password}')
+            print(f'email: {user.email}')
             auth.login(request, user)
 
             return redirect('login')
@@ -27,7 +33,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            return redirect('afterLogin')
         else:
             return render(request, "login.html", {
                 'error': 'username or password is incorrect.',
@@ -41,5 +47,16 @@ def logout(request):
         return redirect('home')
     return render(request,'login.html')
     
+def afterLogin(request):
+    return render(request, 'afterLogin.html', {
+        'user_is_logged_in': request.user.is_authenticated,
+        'username': request.user.username,
+    })
+
+
+# def home(request):
+#     return render(request, 'home.html') 
+
 def home(request):
     return render(request, 'home.html')
+
